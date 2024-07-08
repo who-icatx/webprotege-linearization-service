9d3de215ebbde5bfa9c7c96dccb85ca55417ab7c
alexsilaghi <silaghialexmihail@gmail.com>
2024-07-08 14:18:41 +0300

    moved type on the interface

--- src/main/java/edu/stanford/protege/webprotege/initialrevisionhistoryservice/events/LinearizationEvent.java
+++ src/main/java/edu/stanford/protege/webprotege/initialrevisionhistoryservice/events/LinearizationEvent.java
@@ -1,5 +1,9 @@
 package edu.stanford.protege.webprotege.initialrevisionhistoryservice.events;
 
+
+import org.bson.codecs.pojo.annotations.BsonDiscriminator;
+
+@BsonDiscriminator(key = "type")
 public interface LinearizationEvent {
 
     String getType();
--- src/main/java/edu/stanford/protege/webprotege/initialrevisionhistoryservice/events/LinearizationSpecificationEvent.java
+++ src/main/java/edu/stanford/protege/webprotege/initialrevisionhistoryservice/events/LinearizationSpecificationEvent.java
@@ -1,10 +1,8 @@
 package edu.stanford.protege.webprotege.initialrevisionhistoryservice.events;
 
-import org.bson.codecs.pojo.annotations.BsonDiscriminator;
 import org.semanticweb.owlapi.model.IRI;
 
 
-@BsonDiscriminator(key = "type")
 public abstract class LinearizationSpecificationEvent implements LinearizationEvent {
 
     private final IRI linearizationView;
