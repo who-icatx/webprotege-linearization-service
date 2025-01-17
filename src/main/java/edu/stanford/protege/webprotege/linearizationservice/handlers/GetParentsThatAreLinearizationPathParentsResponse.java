0811ec61515c67691a0ef01191a2d71734200fb4
soimugeo <soimugeo@gmail.com>
2025-01-17 18:11:24 +0200

    fix for https://github.com/who-icatx/icatx-project/issues/111. finished ChangeEntityParentsResult

--- src/main/java/edu/stanford/protege/webprotege/linearizationservice/handlers/GetParentsThatAreLinearizationPathParentsResponse.java
+++ src/main/java/edu/stanford/protege/webprotege/linearizationservice/handlers/GetParentsThatAreLinearizationPathParentsResponse.java
@@ -5,10 +5,10 @@ import com.fasterxml.jackson.annotation.JsonTypeName;
 import edu.stanford.protege.webprotege.common.Response;
 import org.semanticweb.owlapi.model.IRI;
 
-import java.util.Optional;
+import java.util.*;
 
-@JsonTypeName(GetParentThatIsLinearizationPathParentRequest.CHANNEL)
-public record GetParentThatIsLinearizationPathParentResponse(
-        @JsonProperty("parentAsLinearizationPathParent") Optional<IRI> parentAsLinearizationPathParent
+@JsonTypeName(GetParentsThatAreLinearizationPathParentsRequest.CHANNEL)
+public record GetParentsThatAreLinearizationPathParentsResponse(
+        @JsonProperty("parentsThatAreLinearizationPathParents") Set<IRI> parentsThatAreLinearizationPathParents
 ) implements Response {
 }
