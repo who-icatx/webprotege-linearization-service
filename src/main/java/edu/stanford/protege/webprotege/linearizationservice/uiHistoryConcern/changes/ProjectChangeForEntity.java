e3e4fbd626307d1ee3ed7ef0764aaba548ef8843
soimugeo <soimugeo@gmail.com>
2025-05-07 18:01:46 +0300

    [https://github.com/who-icatx/icatx-project/issues/198]: refactored ProjectChangeForEntity to include EntityType.

--- src/main/java/edu/stanford/protege/webprotege/linearizationservice/uiHistoryConcern/changes/ProjectChangeForEntity.java
+++ src/main/java/edu/stanford/protege/webprotege/linearizationservice/uiHistoryConcern/changes/ProjectChangeForEntity.java
@@ -2,22 +2,16 @@ package edu.stanford.protege.webprotege.linearizationservice.uiHistoryConcern.ch
 
 import edu.stanford.protege.webprotege.change.ProjectChange;
 import org.jetbrains.annotations.NotNull;
+import org.semanticweb.owlapi.model.EntityType;
 
 public record ProjectChangeForEntity(String whoficEntityIri,
                                      ChangeType changeType,
+                                     EntityType entityType,
                                      ProjectChange projectChange) implements Comparable<ProjectChangeForEntity> {
 
     public static ProjectChangeForEntity create(String whoficEntityIri,
-                                                ChangeType changeType,
                                                 ProjectChange projectChange) {
-        return new ProjectChangeForEntity(whoficEntityIri, changeType, projectChange);
-    }
-
-    //All linearization/postcoordination changes are updates made on the whoficEntityIri.
-    // From this microservice we don't create or delete entities. that is the responsibility of the backend-service.
-    public static ProjectChangeForEntity create(String whoficEntityIri,
-                                                ProjectChange projectChange) {
-        return new ProjectChangeForEntity(whoficEntityIri, ChangeType.UPDATE_ENTITY, projectChange);
+        return new ProjectChangeForEntity(whoficEntityIri, ChangeType.UPDATE_ENTITY, EntityType.CLASS, projectChange);
     }
 
     @Override
