31fc4552f45bdb071dbddadfaa932e83c6ab1ed6
alexsilaghi <silaghialexmihail@gmail.com>
2025-08-04 14:42:50 +0300

    added new parents changed event.

--- src/main/java/edu/stanford/protege/webprotege/linearizationservice/handlers/SaveEntityLinearizationCommandHandler.java
+++ src/main/java/edu/stanford/protege/webprotege/linearizationservice/handlers/SaveEntityLinearizationCommandHandler.java
@@ -1,19 +1,26 @@
 package edu.stanford.protege.webprotege.linearizationservice.handlers;
 
+import edu.stanford.protege.webprotege.common.EventId;
 import edu.stanford.protege.webprotege.ipc.*;
+import edu.stanford.protege.webprotege.linearizationservice.events.ParentsChangedEvent;
 import edu.stanford.protege.webprotege.linearizationservice.services.LinearizationHistoryService;
+import edu.stanford.protege.webprotege.linearizationservice.uiHistoryConcern.changes.PackagedProjectChangeEvent;
 import org.jetbrains.annotations.NotNull;
 import reactor.core.publisher.Mono;
+
+import java.util.Collections;
 
 @WebProtegeHandler
 public class SaveEntityLinearizationCommandHandler implements CommandHandler<SaveEntityLinearizationRequest, SaveEntityLinearizationResponse> {
 
 
     private final LinearizationHistoryService linearizationHistoryService;
+    private final EventDispatcher eventDispatcher;
 
 
-    public SaveEntityLinearizationCommandHandler(LinearizationHistoryService linearizationHistoryService) {
+    public SaveEntityLinearizationCommandHandler(LinearizationHistoryService linearizationHistoryService, EventDispatcher eventDispatcher) {
         this.linearizationHistoryService = linearizationHistoryService;
+        this.eventDispatcher = eventDispatcher;
     }
 
 
@@ -33,6 +40,8 @@ public class SaveEntityLinearizationCommandHandler implements CommandHandler<Sav
 
         linearizationHistoryService.addRevision(request.entityLinearization(),executionContext, request.projectId(), executionContext.userId(), request.changeRequestId(), request.commitMessage());
 
+        EventId eventId = EventId.generate();
+        eventDispatcher.dispatchEvent(new PackagedProjectChangeEvent(request.projectId(), eventId, Collections.singletonList(new ParentsChangedEvent(request.projectId(), eventId, request.entityLinearization().entityIRI()))));
 
         return Mono.just(SaveEntityLinearizationResponse.create());
     }
